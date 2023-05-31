30ea5e3ab8ec3ad7eaefb6a3266b160524643552
kkjh9909 <kkjh990957@gmail.com>
2023-05-31 22:11:22 +0900

    알림 메시지 수신 구현

--- my-app/src/store/Context.js
+++ my-app/src/store/Context.js
@@ -1,13 +1,34 @@
 import React, {createContext, useEffect, useState} from "react";
+import jwt_decode from 'jwt-decode'
 
 const Context = createContext({})
 
 const ContextProvider = ({children}) => {
+
+	const [notification, setNotification] = useState([]);
 	const [isLogin, setIsLogin] = useState(false);
 
 	useEffect(() => {
-		if(localStorage.getItem('token'))
+		if(localStorage.getItem('token')) {
 			setIsLogin(true);
+
+			const token = localStorage.getItem('token');
+			const payload = jwt_decode(token);
+
+			const eventSource = new EventSource(`http://34.215.66.235:8000/connect/${payload.uid}`);
+
+			eventSource.onmessage = async (event) => {
+				const res = await event.data;
+
+				console.log(res);
+			}
+
+			eventSource.onerror = (event) => {
+				console.log('error', event)
+				eventSource.close();
+			}
+		}
+
 	}, [isLogin])
 
 	return (
